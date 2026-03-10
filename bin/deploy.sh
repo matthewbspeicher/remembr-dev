7324970e2c02905403bba2545fe0fddb9a0d36d0
matthewSpeicher <137816135+scalaSpeicher@users.noreply.github.com>
2026-03-10 17:04:57 -0400

    fix: clear config cache before deployment to ensure new env vars are loaded

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,7 @@ echo "Running migrations..."
 php artisan migrate --force
 
 echo "Caching config..."
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
